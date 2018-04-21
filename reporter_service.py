18d63c44cd6195387139c47b20888c620dbc8c61
mirespace <35487509+mirespace@users.noreply.github.com>
2018-04-21 02:09:09 +0200

    Update reporter_service.py

--- reporter_service.py
+++ reporter_service.py
@@ -49,17 +49,18 @@ class Reporter(object):
         user = UserGithub("mirespace@gmail.com", "5de7de97")
         print "Logged"
         if len(self.list_of_repositories) > 0:
-            list_of_issues = {}
+            self.list_of_issues = {}
             print "Repositories: {number}".format(number=len(self.list_of_repositories))
             for repo_name in self.list_of_repositories:
                 issues = user.get_repo_issues(repo_name)
                 for issue_to_handle in issues:
                     issue = self.convert_to_report_issue(issue_to_handle, repo_name)
-                    self.add_issue_to_list(list_of_issues, issue)
+                    self.add_issue_to_list( issue)
                     
                     
             sorted_list = sorted(list_of_issues.items(), key=operator.itemgetter(0))
             top_day, issues_per_repo = self.extract_top_day_details(sorted_list)
+            return self.create_json_report(top_day, issues_per_repo)
             
         else:
             pass
@@ -75,19 +76,23 @@ class Reporter(object):
     def convert_to_report_issue(issue, repo):
         return Issue(issue["id"], issue["state"], issue["title"], repo,  issue["created_at"]))
     
-    @staticmethod
-    def add_issue_to_list(list_of_issues, issue):
+    def add_issue_to_list(self, issue):
         timestamp =convert_to_timestamp(issue.created_at)
-        if timestamp in list_of_issues:
-            list_of_issues[timestamp].append(issue)
+        if timestamp in self.list_of_issues:
+            self.list_of_issues[timestamp].append(issue)
         else:
-            list_of_issues[timestamp] =(issue,)
+            self.list_of_issues[timestamp] =(issue,)
     
     @staticmethod
     def convert_to_timestamp(datetime)
-        return datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p') #TODO
+        return datetime.strptime(datetime, '%b %d %Y %I:%M%p') #TODO
                     
     @staticmethod
     def extract_top_day_details(sorted_list):
-        pass #TODO            
+        pass #TODO 
+
+    @staticmethod
+    def create_json_report(top_day, issues_per_repo):
+        pass #TODO
+
                     
