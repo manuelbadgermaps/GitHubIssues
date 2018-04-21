537b28fd19f1d2efe402eb6675cea7123e04a2a1
mirespace <35487509+mirespace@users.noreply.github.com>
2018-04-21 02:23:35 +0200

    Update reporter_service.py

--- reporter_service.py
+++ reporter_service.py
@@ -1,25 +1,29 @@
 # -*- coding: utf-8 -*-
 import datetime
+import operator
 
-from repository import Repository
-from github import Github #pip install pygithub
+from github import Github  # pip install pygithub
 
-#https://github.com/PyGithub/PyGithub
-#http://pygithub.readthedocs.io/en/latest/introduction.html
+
+# https://github.com/PyGithub/PyGithub
+# http://pygithub.readthedocs.io/en/latest/introduction.html
 
 class Report(object):
-    name=""
-    number_of_issues=None
-    issues=None
+    name = ""
+    number_of_issues = None
+    issues = None
+
 
 class UserGithub(object):
-    github_connection=None
+    github_connection = None
 
-    def __init__(self,user, password):
-        self.github_connection=Github(user,password, per_page=10000)
+    def __init__(self, user, password):
+        self.github_connection = Github(user, password, per_page=10000)
 
     def get_repo_issues(self, repository):
-        return self.github_connection.get_repo(repository).get_issues(since=(datetime.datetime.today() - datetime.timedelta(days=7)))
+        return self.github_connection.get_repo(repository).get_issues(
+            since=(datetime.datetime.today() - datetime.timedelta(days=7)))
+
 
 class Issue(object):
     id = None
@@ -55,13 +59,12 @@ class Reporter(object):
                 issues = user.get_repo_issues(repo_name)
                 for issue_to_handle in issues:
                     issue = self.convert_to_report_issue(issue_to_handle, repo_name)
-                    self.add_issue_to_list( issue)
-                    
-                    
-            sorted_list = sorted(list_of_issues.items(), key=operator.itemgetter(0))
+                    self.add_issue_to_list(issue)
+
+            sorted_list = sorted(self.list_of_issues.items(), key=operator.itemgetter(0))
             top_day, issues_per_repo = self.extract_top_day_details(sorted_list)
             return self.create_json_report(top_day, issues_per_repo)
-            
+
         else:
             pass
 
@@ -71,28 +74,30 @@ class Reporter(object):
             pass
 
     pass
-    
+
     @staticmethod
     def convert_to_report_issue(issue, repo):
-        return Issue(issue["id"], issue["state"], issue["title"], repo,  issue["created_at"]))
-    
+        return Issue(issue.id, issue.state, issue.title, repo, issue.created_at)
+
+
     def add_issue_to_list(self, issue):
-        timestamp =convert_to_timestamp(issue.created_at)
+
+        timestamp = self.convert_to_timestamp(issue.created_at)
         if timestamp in self.list_of_issues:
             self.list_of_issues[timestamp].append(issue)
         else:
-            self.list_of_issues[timestamp] =(issue,)
-    
+            self.list_of_issues[timestamp] = (issue,)
+
     @staticmethod
-    def convert_to_timestamp(datetime)
-        return datetime.strptime(datetime, '%b %d %Y %I:%M%p') #TODO
-                    
+    def convert_to_timestamp(datetime):
+        print datetime
+        return datetime.strptime(str(datetime), '%Y-%m-%d %H:%M:%S')  # TODO
+
     @staticmethod
     def extract_top_day_details(sorted_list):
-        pass #TODO 
+        return 1, 2
+        pass  # TODO
 
     @staticmethod
     def create_json_report(top_day, issues_per_repo):
-        pass #TODO
-
-                    
+        pass  # TODO
