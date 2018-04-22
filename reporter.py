e861881d16d4eb54dca86983ef6b9e467d9657c3
mirespace <35487509+mirespace@users.noreply.github.com>
2018-04-22 15:45:14 +0200

    Update reporter.py

--- reporter.py
+++ reporter.py
@@ -5,8 +5,8 @@ import sys
 
 arguments = sys.argv
 
-if len(arguments) > 1:
+if len(arguments) > 3:
     reporter = Reporter(arguments[1])
-    print reporter.run()
+    print reporter.run(arguments[2],arguments[3])
 else:
-    print("Usage python reporter.py list_of_repositories")
+    print("Usage: python reporter.py github_username github_password list_of_repositories")
