81912973a95d5b2e6a6f69a7d8136146cd63620e
mirespace <35487509+mirespace@users.noreply.github.com>
2018-04-22 03:41:22 +0200

    Update reporter.py

--- reporter.py
+++ reporter.py
@@ -6,7 +6,7 @@ import sys
 arguments = sys.argv
 
 if len(arguments) > 1:
-    reporter=Reporter(arguments[1])
+    reporter = Reporter(arguments[1])
     print reporter.run()
 else:
     print("Usage python reporter.py list_of_repositories")
